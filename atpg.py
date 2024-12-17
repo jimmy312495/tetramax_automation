8a9046315bba947cfd84160fa4124f267be98019
jimmyb08901017 <jimmy312495@gmail.com>
2024-12-18 03:31:51 +0800

    remove redundant write_

--- atpg.py
+++ atpg.py
@@ -5,7 +5,7 @@ class BaseATPGScriptGenerator:
     def __init__(self, config: ATPGConfig):
         self.config = config
     
-    def write_set(self, file):
+    def set(self, file):
         file.write("""##############################################
 #                   ATPG                     #
 ##############################################\n\n""")
@@ -22,7 +22,7 @@ class BaseATPGScriptGenerator:
         # Run DRC
         file.write(f"run_drc {self.config.spf_file}\n\n")
 
-    def write_set_atpg(self, file):
+    def set_atpg(self, file):
         # Set ATPG
         file.write("set_atpg -coverage 100\n")
         
@@ -30,15 +30,15 @@ class BaseATPGScriptGenerator:
             file.write("set_atpg -fill X\n")
         
         file.write("set_atpg -decision random\n")
-        self.write_set_atpg_option(file)
+        self.set_atpg_option(file)
     
-    def write_set_atpg_option(self, file):
+    def set_atpg_option(self, file):
         # inheritance
         pass
 
-    def write_set_fault(self, file):
+    def set_fault(self, file):
         # Set fault model
-        self.write_set_fault_option(file)
+        self.set_fault_option(file)
         
         if self.config.fault_collapsing:
             file.write("set_faults -report collapsed\n") 
@@ -46,11 +46,11 @@ class BaseATPGScriptGenerator:
         file.write("set_faults -fault_coverage\n")
         file.write("set_faults -summary verbose\n\n")
         
-    def write_set_fault_option(self, file):
+    def set_fault_option(self, file):
         # default: Stuck
         file.write("set_faults -model Stuck\n")
 
-    def write_set_delay_option(self, file):
+    def set_delay_option(self, file):
         pass
         
     def write_output(self, file):
@@ -60,11 +60,11 @@ class BaseATPGScriptGenerator:
         file.write(f"write_patterns {self.config.patterns_file} -format STIL -replace\n\n")
         file.write("exit")
 
-    def write_add_fault(self, file):
+    def add_fault(self, file):
         # Add fault list to ATPG
         file.write("add_faults -all\n\n")
 
-    def write_run_atpg(self, file):
+    def run_atpg(self, file):
         # Run ATPG
         if self.config.auto_compression:
             file.write("run_atpg -auto_compression\n\n") 
@@ -73,19 +73,19 @@ class BaseATPGScriptGenerator:
 
     def generate_tcl(self, output_file):
         with open(output_file, "w") as file:
-            self.write_set(file)
-            self.write_set_atpg(file)
-            self.write_set_fault(file)
-            self.write_set_delay_option(file)
-            self.write_add_fault(file)
-            self.write_run_atpg(file)
+            self.set(file)
+            self.set_atpg(file)
+            self.set_fault(file)
+            self.set_delay_option(file)
+            self.add_fault(file)
+            self.run_atpg(file)
             self.write_output(file)
 
 class StuckATPGScriptGenerator(BaseATPGScriptGenerator):
     def __init__(self, config: ATPGConfig):
         super().__init__(config)
         
-    def write_set_fault_option(self, file):
+    def set_fault_option(self, file):
         # Set fault model
         file.write("set_faults -model Stuck\n")
         
@@ -96,28 +96,28 @@ class TransitionATPGScriptGenerator(BaseATPGScriptGenerator):
             self.config.capture_cycle = 4
         assert(self.config.capture_cycle >= 2 and self.config.capture_cycle <= 10)
         
-    def write_set_fault_option(self, file):
+    def set_fault_option(self, file):
         file.write("set_faults -model transition\n")
     
-    def write_set_delay_option(self, file):
+    def set_delay_option(self, file):
         file.write("set_delay -launch system_clock\n\n")
     
-    def write_set_atpg_option(self, file):
+    def set_atpg_option(self, file):
         file.write(f"set_atpg -capture {self.config.capture_cycle}\n\n")
         
 class BridgingATPGScriptGenerator(BaseATPGScriptGenerator):
     def __init__(self, config: ATPGConfig):
         super().__init__(config)
         
-    def write_set_fault_option(self, file):
+    def set_fault_option(self, file):
         # Set fault model
         file.write("set_faults -model transition\n")
     
-    def write_add_fault(self, file):
+    def add_fault(self, file):
         # override default
         file.write("add_faults -node_file nodes.txt \n\n")
     
-    def write_set_atpg_option(self, file):
+    def set_atpg_option(self, file):
         # Set ATPG
         file.write("set_atpg -merge high \n")
 
