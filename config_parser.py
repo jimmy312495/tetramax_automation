3365a22a10ada4da3ffef3766823220e79576e19
jimmyb08901017 <jimmy312495@gmail.com>
2024-12-24 15:30:20 +0800

    refine config and its parser

--- config_parser.py
+++ config_parser.py
@@ -1,48 +1,43 @@
 import configparser
 from typing import Optional
 
-# [ DEFAULT ] 
-# top_module = s1423
-# netlist_file = ./Netlist/s1423_dft.v
+# [DEFAULT]
+# top_module = s15850
+# netlist_file = ./Netlist/s15850_dft.v
 # tech_library =  /home/raid7_2/course/cvsd/CBDK_IC_Contest_v2.5/Verilog/tsmc13_neg.v
 # db_library = /home/raid7_2/course/cvsd/CBDK_IC_Contest/CIC/SynopsysDC/db
-# synthesized_files = ../Test_s1423/Netlist/s1423_syn.v
-# spf_file = ../Test_s1423/Netlist/s1423.spf
-# spef_file = ../Test_s1423/Netlist/s1423.spef
-# faults_file = ../Test_s1423/Netlist/s1423.fault
-# summary_file = ../Test_s1423/Netlist/s1423_ATPG_report.rpt
-# patterns_file = ../Test_s1423/Netlist/s1423.stil
+# synthesized_files = ../Test_s15850/Netlist/s15850_syn.v
+# spf_file = ../Test_s15850/Netlist/s15850_dft.spf
+# spef_file = ../Test_s15850/Netlist/s15850_dft.spef 
+# faults_file = ../Test_s15850/Netlist/s15850.fault
+# summary_file = ../Test_s15850/Netlist/s15850_report.rpt
+# patterns_file = ../Test_s15850/Netlist/s15850.stil
 
 # [SCAN_CHAIN_INSERT]
 # # supported scan chain type: <multiplexed_flip_flop | clocked_scan | lssd | aux_clock_lssd | combinational | or none>
 # scan_style = multiplexed_flip_flop
 # num_scan_chain = 8
 
+# [SCAN_CHAIN_FAULT]
 # # detect scan chain fault
-# [ SCAN_CHAIN_FAULT ]
 # scan_fault_detect = false
 
-# # supoorted fault types: stuck, transition, iddq, iddq_bridging, path_delay, hold_time, bridging, dynamic_bridging, dual_transition_bridging
-# [ FAULT_TYPES ]
+# # supoorted fault types: stuck, transition, iddq, path_delay, hold_time, bridging
+# [FAULT_TYPES]
 # fault_model = transition
 
-# [ PATTERN_OPTIONS ]
+# [PATTERN_OPTIONS]
 # pattern_specification = full
 # fault_collapsing = true
+# fault_coverage = 100
 
-# [ TRANSITION_FAULT_OPTIONS ]
+# [STUCK_FAULT_OPTIONS]
+# N_detect = 1
+
+# [TRANSITION_FAULT_OPTIONS]
 # launch_cycle = any
-# capture_cycle = 2
+# capture_cycle = 4
 # MUXClock_mode = false
-
-# [ ATPG_GENERAL_OPTIONS ]
-# auto_compression = true
-# # optional: remove p% faults
-# remove_fault = 50
-
-# [SIMULATION_OPTION]
-# simulation_sequential = true
-# simulation_sequential_nodrop = false
 
 # [IDDQ_FAULT_OPTIONS]
 # iddq_max_patterns = 20
@@ -51,11 +46,21 @@ from typing import Optional
 # iddq_strong = true
 # iddq_interval_size = 1
 
-# [PATH_DELAY_FAULT_OPTIONS]
-# slack = 0.15
+# [BRIDGING_FAULT_OPTIONS]
+# bridging_optimize_bridge_strengths = true
 
-# [STUCK_FAULT_OPTIONS]
-# N_detect = 1
+# [PATH_DELAY_FAULT_OPTIONS]
+# path_delay_slack = 0.15
+# path_delay_max_paths = 200
+
+# [ATPG_GENERAL_OPTIONS]
+# auto_compression = true
+# # optional: remove p% faults
+# # remove_fault = 50
+
+# [SIMULATION_OPTION]
+# simulation_sequential = true
+# simulation_sequential_nodrop = false
 
 class Config:
     def __init__(self, 
@@ -65,7 +70,7 @@ class Config:
                  db_library: str,
                  synthesized_files: str, 
                  spf_file: str,
-                 spef_file: str,
+                 spef_file: Optional[str],
                  faults_file: str, 
                  summary_file: str, 
                  patterns_file: str,
@@ -74,11 +79,11 @@ class Config:
                  scan_fault_detect: bool, 
                  fault_model: str, 
                  pattern_specification: str, 
-                 fault_collapsing: bool,  # Changed from Optional[bool]
+                 fault_collapsing: bool,
                  launch_cycle: Optional[str],
                  capture_cycle: Optional[int],
-                 MUXClock_mode: bool,     # Changed from Optional[int]
-                 auto_compression: bool,   # Changed from Optional[int]
+                 MUXClock_mode: bool,
+                 auto_compression: bool,
                  remove_fault: Optional[int],
                  simulation_sequential: bool,
                  simulation_sequential_nodrop: bool,
@@ -88,7 +93,13 @@ class Config:
                  iddq_strong: bool = True,
                  iddq_interval_size: int = 1,
                  n_detect: int = 1,
-                 slack: float = 0.15):
+                 path_delay_slack: float = 0.15,
+                 bridging_optimize_bridge_strengths: bool = True,
+                 path_delay_max_paths: int = 200,
+                 fault_coverage: int = 100):
+        # error detect
+        if not all([top_module, netlist_file, tech_library, db_library, synthesized_files, spf_file, faults_file, summary_file, patterns_file]):
+            raise ValueError("All required parameters must be non-empty strings.")
         self.top_module = top_module
         self.netlist_file = netlist_file
         self.tech_library = tech_library
@@ -118,7 +129,10 @@ class Config:
         self.iddq_strong = iddq_strong
         self.iddq_interval_size = iddq_interval_size
         self.n_detect = n_detect
-        self.slack = slack
+        self.path_delay_slack = path_delay_slack
+        self.bridging_optimize_bridge_strengths = bridging_optimize_bridge_strengths
+        self.path_delay_max_paths = path_delay_max_paths
+        self.fault_coverage = fault_coverage
 
     def __repr__(self):
         return (f"ATPGConfig(top_module={self.top_module}, netlist_file={self.netlist_file}, tech_library={self.tech_library}, "
@@ -134,7 +148,8 @@ class Config:
                 f"iddq_max_patterns={self.iddq_max_patterns}, iddq_toggle={self.iddq_toggle}, "
                 f"iddq_float={self.iddq_float}, iddq_strong={self.iddq_strong}, "
                 f"iddq_interval_size={self.iddq_interval_size}, n_detect={self.n_detect}, "
-                f"slack={self.slack})")
+                f"path_delay_slack={self.path_delay_slack}, bridging_optimize_bridge_strengths={self.bridging_optimize_bridge_strengths}, "
+                f"path_delay_max_paths={self.path_delay_max_paths}, fault_coverage={self.fault_coverage})")
 
 def parse_config(file_path: str) -> Config:
     config = configparser.ConfigParser()
@@ -158,6 +173,7 @@ def parse_config(file_path: str) -> Config:
     iddq_section = config["IDDQ_FAULT_OPTIONS"]
     path_delay_section = config["PATH_DELAY_FAULT_OPTIONS"]
     stuck_section = config["STUCK_FAULT_OPTIONS"]
+    bridging_section = config["BRIDGING_FAULT_OPTIONS"]
 
     return Config(
         # DEFAULT section
@@ -174,7 +190,7 @@ def parse_config(file_path: str) -> Config:
         
         # SCAN_CHAIN_INSERT section
         scan_style=scan_chain_section.get("scan_style", "multiplexed_flip_flop"),
-        num_scan_chain=int(scan_chain_section.get("num_scan_chain", "8")),
+        num_scan_chain=scan_chain_section.getint("num_scan_chain", 8),
         
         # SCAN_CHAIN_FAULT section
         scan_fault_detect=parse_bool(scan_fault_section.get("scan_fault_detect")),
@@ -183,34 +199,39 @@ def parse_config(file_path: str) -> Config:
         fault_model=fault_types_section.get("fault_model", "stuck"),
         
         # PATTERN_OPTIONS section
-        pattern_specification=pattern_section.get("pattern_specification", ""),
-        fault_collapsing=parse_bool(pattern_section.get("fault_collapsing")),
+        pattern_specification=pattern_section.get("pattern_specification", "full"),
+        fault_collapsing=parse_bool(pattern_section.get("fault_collapsing", "true")),
+        fault_coverage=pattern_section.getint("fault_coverage", 100),
         
         # TRANSITION_FAULT_OPTIONS section
         launch_cycle=transition_section.get("launch_cycle", "any"),
-        capture_cycle=parse_optional_int(transition_section.get("capture_cycle", "4")),
+        capture_cycle=transition_section.getint("capture_cycle", 4),
         MUXClock_mode=parse_bool(transition_section.get("MUXClock_mode", "false")),
         
         # ATPG_GENERAL_OPTIONS section
-        auto_compression=parse_bool(general_section.get("auto_compression")),
-        remove_fault=parse_optional_int(general_section.get("remove_fault")),
+        auto_compression=parse_bool(general_section.get("auto_compression", "true")),
+        remove_fault=parse_optional_int(general_section.get("remove_fault", "0")),
 
         # SIMULATION_OPTION section
-        simulation_sequential=parse_bool(simulation_section.get("simulation_sequential")),
-        simulation_sequential_nodrop=parse_bool(simulation_section.get("simulation_sequential_nodrop")),
+        simulation_sequential=parse_bool(simulation_section.get("simulation_sequential", "false")),
+        simulation_sequential_nodrop=parse_bool(simulation_section.get("simulation_sequential_nodrop", "false")),
 
         # IDDQ_FAULT_OPTIONS section
         iddq_max_patterns=iddq_section.getint("iddq_max_patterns", 1000),
-        iddq_toggle=parse_bool(iddq_section.get("iddq_toggle")),
-        iddq_float=parse_bool(iddq_section.get("iddq_float")),
-        iddq_strong=parse_bool(iddq_section.get("iddq_strong")),
+        iddq_toggle=parse_bool(iddq_section.get("iddq_toggle", "true")),
+        iddq_float=parse_bool(iddq_section.get("iddq_float", "true")),
+        iddq_strong=parse_bool(iddq_section.get("iddq_strong", "true")),
         iddq_interval_size=iddq_section.getint("iddq_interval_size", 1),
 
         # STUCK_FAULT_OPTIONS section
         n_detect=stuck_section.getint("N_detect", 1),
 
         # PATH_DELAY_FAULT_OPTIONS section
-        slack=float(path_delay_section.get("slack", "0.15"))
+        path_delay_slack=float(path_delay_section.get("path_delay_slack", "0.15")),
+        path_delay_max_paths=path_delay_section.getint("path_delay_max_paths", 200),
+
+        # BRIDGING_FAULT_OPTIONS section
+        bridging_optimize_bridge_strengths=parse_bool(bridging_section.get("bridging_optimize_bridge_strengths", "true"))
     )
 
 def main():
